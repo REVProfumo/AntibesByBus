824e2548c184feacf77b19042c342e60bbcf6ab0
Elio <rosarioelio.profumo@cea.fr>
2015-09-21 01:36:05 +0200

    added starting of the app Formulas

---
+++ Formulas/app/src/main/java/com/mycompany/formulas/New_set.java
@@ -0,0 +1,19 @@
+package com.mycompany.formulas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by elio-profumo on 21/09/15.
+ */
+public class New_set extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_set);
+
+    }
+}
---
+++ Formulas/app/src/main/java/com/mycompany/formulas/Show_sets.java
@@ -0,0 +1,17 @@
+package com.mycompany.formulas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by elio-profumo on 21/09/15.
+ */
+public class Show_sets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_sets);
+
+    }
+}
