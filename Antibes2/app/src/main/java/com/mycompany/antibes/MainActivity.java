9f4bd22c048075eb23bece4e30ecf4b9d014dc68
Elio <rosarioelio.profumo@cea.fr>
2015-11-02 23:16:19 +0100

    added table in background

--- Antibes2/app/src/main/java/com/mycompany/antibes/MainActivity.java
+++ Antibes2/app/src/main/java/com/mycompany/antibes/MainActivity.java
@@ -18,6 +18,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.text.TextUtils;
 import android.widget.Toast;
@@ -254,7 +256,56 @@ public class MainActivity extends AppCompatActivity {
         params.addRule(RelativeLayout.BELOW, tv.getId());
         myLayout.addView(textview, params);
 
+
+            TableLayout stk = (TableLayout) findViewById(R.id.table_main);
+            TableRow tbrow0 = new TableRow(this);
+            TextView tv0 = new TextView(this);
+            tv0.setText(" Sl.No ");
+            tv0.setTextColor(Color.WHITE);
+            tbrow0.addView(tv0);
+            TextView tv1 = new TextView(this);
+            tv1.setText(" Product ");
+            tv1.setTextColor(Color.WHITE);
+            tbrow0.addView(tv1);
+            TextView tv2 = new TextView(this);
+            tv2.setText(" Unit Price ");
+            tv2.setTextColor(Color.WHITE);
+            tbrow0.addView(tv2);
+            TextView tv3 = new TextView(this);
+            tv3.setText(" Stock Remaining ");
+            tv3.setTextColor(Color.WHITE);
+            tbrow0.addView(tv3);
+            stk.addView(tbrow0);
+            for (int i = 0; i < 25; i++) {
+                TableRow tbrow = new TableRow(this);
+                TextView t1v = new TextView(this);
+                t1v.setText("" + i);
+                t1v.setTextColor(Color.WHITE);
+                t1v.setGravity(Gravity.CENTER);
+                tbrow.addView(t1v);
+                TextView t2v = new TextView(this);
+                t2v.setText("Product " + i);
+                t2v.setTextColor(Color.WHITE);
+                t2v.setGravity(Gravity.CENTER);
+                tbrow.addView(t2v);
+                TextView t3v = new TextView(this);
+                t3v.setText("Rs." + i);
+                t3v.setTextColor(Color.WHITE);
+                t3v.setGravity(Gravity.CENTER);
+                tbrow.addView(t3v);
+                TextView t4v = new TextView(this);
+                t4v.setText("" + i * 15 / 32 * 10);
+                t4v.setTextColor(Color.WHITE);
+                t4v.setGravity(Gravity.CENTER);
+                tbrow.addView(t4v);
+                stk.addView(tbrow);
+
+        }
+
+
     }
+
+
 
     public void lookUp(View v) {
         ViewGroup layout = (ViewGroup) findViewById(R.id.content_main);
