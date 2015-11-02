2ab51325b78b5cdf0c2d6a5cd2f70a8a0a8f7902
Elio <rosarioelio.profumo@cea.fr>
2015-11-02 23:56:37 +0100

    added table

--- Antibes2/app/src/main/java/com/mycompany/antibes/MainActivity.java
+++ Antibes2/app/src/main/java/com/mycompany/antibes/MainActivity.java
@@ -159,6 +159,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void generateTextView(Cursor cursor){
 
+        TableLayout stk = (TableLayout) findViewById(R.id.table_main);
+        TableRow tbrow0 = new TableRow(this);
+        TextView tv0 = new TextView(this);
+        tv0.setText(" Line ");
+        tv0.setTextColor(Color.WHITE);
+        tbrow0.addView(tv0);
+        TextView tv1 = new TextView(this);
+        tv1.setText(" Direction ");
+        tv1.setTextColor(Color.WHITE);
+        tbrow0.addView(tv1);
+        TextView tv2 = new TextView(this);
+        tv2.setText(" time ");
+        tv2.setTextColor(Color.WHITE);
+        tbrow0.addView(tv2);
+        stk.addView(tbrow0);
+
         String resultSchedule = "";
 
         int iRow = cursor.getColumnIndex(FeedReaderContract.FeedEntry.STOP);
@@ -218,12 +234,12 @@ public class MainActivity extends AppCompatActivity {
                 } catch (NumberFormatException nfe) {
                 };
             }
-
+            String nextTimesChrono ="";
+            String newiName="";
             int minsNext = 0;
-
             if (flag == 1) {
                 String nextTimesString="";
-                String nextTimesChrono ="";
+
                 for (int i = 0; i < 2; i++) {
                     int hours = nextTimes[i]/3600;
                     int mins = (nextTimes[i]-hours*3600)/60;
@@ -245,15 +261,34 @@ public class MainActivity extends AppCompatActivity {
 
                     nextTimesString += Integer.toString(nextTimes[i]) + " ";
                 }
-                String newiName = cursor.getString(iName).replace('+',' ');
+                newiName += cursor.getString(iName).replace('+',' ');
 
                 resultSchedule = resultSchedule + auxString + " " + newiName +
                         " " +  cursor.getString(iDirection)+
                         " " + nextTimesChrono + "(next in "+ Integer.toString(minsNext) + " mins)" +
                         "\n";
             }
-        }
 
+
+            TableRow tbrow = new TableRow(this);
+            TextView t1v = new TextView(this);
+            t1v.setText(cursor.getString(iDirection));
+            t1v.setTextColor(Color.WHITE);
+            t1v.setGravity(Gravity.CENTER);
+            tbrow.addView(t1v);
+            TextView t2v = new TextView(this);
+            t2v.setText(newiName);
+            t2v.setTextColor(Color.WHITE);
+            t2v.setGravity(Gravity.CENTER);
+            tbrow.addView(t2v);
+            TextView t3v = new TextView(this);
+            t3v.setText(nextTimesChrono + "(next in "+ Integer.toString(minsNext) + " mins)");
+            t3v.setTextColor(Color.WHITE);
+            t3v.setGravity(Gravity.CENTER);
+            tbrow.addView(t3v);
+            stk.addView(tbrow);
+        }
+        /*
         TextView textview = new TextView(getApplicationContext());
         textview.setText(resultSchedule);
         textview.setId(1);
@@ -266,51 +301,7 @@ public class MainActivity extends AppCompatActivity {
         textview.setTextColor(Color.RED);
         params.addRule(RelativeLayout.BELOW, tv.getId());
         myLayout.addView(textview, params);
-
-
-            TableLayout stk = (TableLayout) findViewById(R.id.table_main);
-            TableRow tbrow0 = new TableRow(this);
-            TextView tv0 = new TextView(this);
-            tv0.setText(" Sl.No ");
-            tv0.setTextColor(Color.WHITE);
-            tbrow0.addView(tv0);
-            TextView tv1 = new TextView(this);
-            tv1.setText(" Product ");
-            tv1.setTextColor(Color.WHITE);
-            tbrow0.addView(tv1);
-            TextView tv2 = new TextView(this);
-            tv2.setText(" Unit Price ");
-            tv2.setTextColor(Color.WHITE);
-            tbrow0.addView(tv2);
-            TextView tv3 = new TextView(this);
-            tv3.setText(" Stock Remaining ");
-            tv3.setTextColor(Color.WHITE);
-            tbrow0.addView(tv3);
-            stk.addView(tbrow0);
-            for (int i = 0; i < 25; i++) {
-                TableRow tbrow = new TableRow(this);
-                TextView t1v = new TextView(this);
-                t1v.setText("" + i);
-                t1v.setTextColor(Color.WHITE);
-                t1v.setGravity(Gravity.CENTER);
-                tbrow.addView(t1v);
-                TextView t2v = new TextView(this);
-                t2v.setText("Product " + i);
-                t2v.setTextColor(Color.WHITE);
-                t2v.setGravity(Gravity.CENTER);
-                tbrow.addView(t2v);
-                TextView t3v = new TextView(this);
-                t3v.setText("Rs." + i);
-                t3v.setTextColor(Color.WHITE);
-                t3v.setGravity(Gravity.CENTER);
-                tbrow.addView(t3v);
-                TextView t4v = new TextView(this);
-                t4v.setText("" + i * 15 / 32 * 10);
-                t4v.setTextColor(Color.WHITE);
-                t4v.setGravity(Gravity.CENTER);
-                tbrow.addView(t4v);
-                stk.addView(tbrow);
-        }
+        */
 
 
     }
