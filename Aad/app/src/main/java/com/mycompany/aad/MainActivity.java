174e5ee4a4874ece1445c6135017c60353189435
Elio <rosarioelio.profumo@cea.fr>
2015-11-06 10:26:29 +0100

    added example of dropmenu

---
+++ Aad/app/src/main/java/com/mycompany/aad/MainActivity.java
@@ -0,0 +1,86 @@
+package com.mycompany.aad;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.internal.view.menu.ActionMenuItemView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuInflater;
+import android.view.SubMenu;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Menu menu_global;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // Override this method to do what you want when the menu is recreated
+
+
+
+        Button buttonX = (Button)findViewById(R.id.button_additem);
+// Register the onClick listener with the implementation above
+        buttonX.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                menu_global.add(0, 1, 0, "Hello");
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        SubMenu menuItem = menu.findItem(R.id.menu_new_form).getSubMenu();
+        this.menu_global = menuItem;
+
+        //the menu option text is defined in resources
+
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
+
+
+
+}
+
